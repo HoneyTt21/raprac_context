67ce14fb8fd708fe3381cd5646bc7013b2155deb
HoneyTt <honeytt21@kaist.ac.kr>
2020-09-15 12:22:12 +0900

    Final : after applying useContext and static contextType

--- src/components/ColorBox.js
+++ src/components/ColorBox.js
@@ -1,30 +1,27 @@
-import React from "react";
-import ColorConsumer from "./Color";
+import React, {useContext} from "react";
+import ColorContext from "./Color";
 
 const ColorBox = () => {
+    const {state} = useContext(ColorContext);
     return (
-        <ColorConsumer>
-            {({state}) => (
-                <>
-                    <div
-                        style={{
-                            width: "64px",
-                            height: "64px",
-                            background: state.color,
-                            border: "3px aqua solid",
-                        }}
-                    />
-                    <div
-                        style={{
-                            width: "32px",
-                            height: "32px",
-                            border: "3px solid aqua",
-                            background: state.subcolor,
-                        }}
-                    />
-                </>
-            )}
-        </ColorConsumer>
+        <>
+            <div
+                style={{
+                    width: "64px",
+                    height: "64px",
+                    background: state.color,
+                    border: "3px aqua solid",
+                }}
+            />
+            <div
+                style={{
+                    width: "32px",
+                    height: "32px",
+                    border: "3px solid aqua",
+                    background: state.subcolor,
+                }}
+            />
+        </>
     );
 };
 
--- src/components/SelectColors.js
+++ src/components/SelectColors.js
@@ -1,37 +1,45 @@
-import React from "react";
-import {ColorConsumer} from "./Color";
+import React, {Component} from "react";
+import ColorContext from "./Color";
 
 const colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
 
-const SelectColors = () => {
-    return (
-        <div>
-            <h2>Select among given colors</h2>
-            <ColorConsumer>
-                {({actions}) => (
-                    <div style={{display: "flex"}}>
-                        {colors.map((color) => (
-                            <div
-                                key={color}
-                                style={{
-                                    background: color,
-                                    width: "24px",
-                                    height: "24px",
-                                    cursor: "pointer",
-                                }}
-                                onClick={() => actions.setColor(color)}
-                                onContextMenu={(e) => {
-                                    e.preventDefault();
-                                    actions.setSubColor(color);
-                                }}
-                            />
-                        ))}
-                    </div>
-                )}
-            </ColorConsumer>
-            <hr />
-        </div>
-    );
-};
+class SelectColors extends Component {
+    static contextType = ColorContext;
+
+    handleSetColor = (color) => {
+        this.context.actions.setColor(color);
+    };
+    handleSetSubColor = (subcolor) => {
+        this.context.actions.setSubColor(subcolor);
+    };
+
+    render() {
+        return (
+            <div>
+                <h2>Select among given colors</h2>
+
+                <div style={{display: "flex"}}>
+                    {colors.map((color) => (
+                        <div
+                            key={color}
+                            style={{
+                                background: color,
+                                width: "24px",
+                                height: "24px",
+                                cursor: "pointer",
+                            }}
+                            onClick={() => this.handleSetColor(color)}
+                            onContextMenu={(e) => {
+                                e.preventDefault();
+                                this.handleSetSubColor(color);
+                            }}
+                        />
+                    ))}
+                </div>
+                <hr />
+            </div>
+        );
+    }
+}
 
 export default SelectColors;
